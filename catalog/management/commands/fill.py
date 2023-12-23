e87c56fc6e5ebd29462f6a00effb211a1a6a9f33
Aleksandr Buganov <rivares89@gmail.com>
2023-12-24 00:52:15 +0300

    20.1

---
+++ catalog/management/commands/fill.py
@@ -0,0 +1,19 @@
+from django.core.management import BaseCommand
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        product_list = [
+            {'name': 'Телевизор', 'category': 'TV'},
+            {'name': 'Монитор', 'category': 'TV'},
+            {'name': 'Телефон', 'category': 'Смартфоны'},
+            {'name': 'Айфон', 'category': 'Смартфоны'},
+        ]
+
+        product_for_create = []
+        for item in product_list:
+            product_for_create.append(
+                Product(**item)
+            )
+
+        Product.objects.bulk_create(product_for_create)
+
--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+NULLABLE = {'blank': True, 'null': True}
+class Product(models.Model):
+    name = models.CharField(max_length=100, verbose_name='название')
+    description = models.CharField(max_length=100, verbose_name='описание')
+    image = models.ImageField(upload_to='products/', verbose_name='изображение', **NULLABLE)
+    category = models.CharField(max_length=100, verbose_name='категория')
+    price_for_one = models.CharField(max_length=100, verbose_name='цена за штуку')
+    date_creation = models.CharField(max_length=100, verbose_name='дата создания')
+    last_modified_data = models.CharField(max_length=100, verbose_name='дата последнего изменения')
+
+    def __str__(self):
+        return f'{self.name} {self.category}'
+
+    class Meta:
+        verbose_name = 'продукт'
+        verbose_name_plural = 'продукты'
+        ordering = ('name',)
+
+class Category(models.Model):
+    name_c = models.CharField(max_length=100, verbose_name='название', default='DEFAULT VALUE')
+    description_c = models.CharField(max_length=100, verbose_name='описание', default='DEFAULT VALUE')
+
+    def __str__(self):
+        return f'{self.name_c} {self.description_c}'
+
+    class Meta:
+        verbose_name = 'категория'
+        verbose_name_plural = 'категории'
+        ordering = ('name_c',)
