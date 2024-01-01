73743e204fb6b429e14272e3cad7c9844db2a36d
Aleksandr Buganov <rivares89@gmail.com>
2024-01-02 02:30:11 +0300

    shabl

--- catalog/views.py
+++ catalog/views.py
@@ -1,14 +1,29 @@
 from django.shortcuts import render
 
-def home(request):
-    return render(request, 'catalog/home.html')
+from catalog.models import Product
 
-def contacts(request):
+
+def home(request):
+    product_list = Product.objects.all()
+    context = {
+        'object_list': product_list,
+        'title': 'Главная'
+    }
+
+    return render(request, 'catalog/home.html', context)
+
+def contact(request):
     if request.method == 'POST':
         name = request.POST.get('name')
         phone = request.POST.get('phone')
         message = request.POST.get('message')
         print(f'You have new message from {name}({phone}): {message}')
-    return render(request, 'catalog/contacts.html')
+
+    context = {
+        'title': 'Контакты'
+    }
+    return render(request, 'catalog/contact.html', context)
+
+
 
 
